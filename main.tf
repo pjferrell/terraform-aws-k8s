2ca75a6681b07a1ca123a8e201fda5c4252fa73a
kkacsh321 <kkacsh@infoblox.com>
2022-06-10 13:31:15 -0600

    DEVOPS Fixing node security groups to meet min

--- main.tf
+++ main.tf
@@ -306,6 +306,65 @@ resource "aws_security_group_rule" "demo-node-ingress-cluster" {
   type                     = "ingress"
 }
 
+# Allow 443 from cluster security group to nodes
+resource "aws_security_group_rule" "node-ingress-cluster-https" {
+  description              = "Allow pods to communicate with the cluster API Server"
+  from_port                = 443
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.node.id
+  source_security_group_id = aws_security_group.cluster.id
+  to_port                  = 443
+  type                     = "ingress"
+}
+
+resource "aws_security_group_rule" "node-engress-cluster-https" {
+  description              = "Allow pods to communicate with the cluster API Server"
+  from_port                = 443
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.node.id
+  source_security_group_id = aws_security_group.cluster.id
+  to_port                  = 443
+  type                     = "egress"
+}
+resource "aws_security_group_rule" "node-ingress-coredns-tcp-https" {
+  description              = "Allow pods to communicate with the cluster API Server"
+  from_port                = 53
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.node.id
+  source_security_group_id = aws_security_group.cluster.id
+  to_port                  = 53
+  type                     = "ingress"
+}
+
+resource "aws_security_group_rule" "node-ingress-coredns-udp-https" {
+  description              = "Allow pods to communicate with the cluster API Server"
+  from_port                = 53
+  protocol                 = "udp"
+  security_group_id        = aws_security_group.node.id
+  source_security_group_id = aws_security_group.cluster.id
+  to_port                  = 53
+  type                     = "ingress"
+}
+
+resource "aws_security_group_rule" "node-egress-coredns-tcp-https" {
+  description              = "Allow pods to communicate with the cluster API Server"
+  from_port                = 53
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.node.id
+  source_security_group_id = aws_security_group.cluster.id
+  to_port                  = 53
+  type                     = "egress"
+}
+
+resource "aws_security_group_rule" "node-egress-coredns-udp-https" {
+  description              = "Allow pods to communicate with the cluster API Server"
+  from_port                = 53
+  protocol                 = "udp"
+  security_group_id        = aws_security_group.node.id
+  source_security_group_id = aws_security_group.cluster.id
+  to_port                  = 53
+  type                     = "egress"
+}
 
 # EKS Master <--> Worker Security Group
 resource "aws_security_group_rule" "cluster-ingress-node-https" {
